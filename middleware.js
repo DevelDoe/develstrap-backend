967e8a36f55d65c206997a8c703ac4259a15c4f0
develdoe <andreeray@live.com>
2018-10-17 19:19:03 +0200

    'u'

--- middleware.js
+++ middleware.js
@@ -35,3 +35,13 @@ function authenticate(req, res, next) {
         })()
     }
 }
+
+function getToken(headers) {
+    if( headers && headers.authorization) {
+        var parted = headers.authorization.split(' ')
+        if( parted.length = 2) return parted[1]
+        else return null
+    } else {
+        return null
+    }
+}

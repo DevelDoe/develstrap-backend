447c9a70bf24cf8e6bcdf1a16282bdff7db8e112
develdoe <andreeray@live.com>
2018-10-17 18:47:36 +0200

    'u'

--- middleware.js
+++ middleware.js
@@ -1,6 +1,6 @@
 var cors        = require('cors'),
     bp          = require('body-parser'),
-    passport    = require('passport'),
+    passport    = require('passport')
 
 module.exports = function( api ) {
     api.use( bp.urlencoded( { extended: true } ) )

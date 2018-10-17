1f5f5cbfd982f947a6d0fb6501163d97eabcf567
develdoe <andreeray@live.com>
2018-10-17 18:33:02 +0200

    'u'

---
+++ passport.js
@@ -0,0 +1,18 @@
+var jwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt  = require('passport-jwt').ExtractJwt
+    User        = require('../models/user.js'),
+    config      = require('../config/database')
+
+module.exports = function( pp ) {
+    var opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt')
+    opts.secretOrKey = config.secret
+    pp.use(new jwtStrategy( opts , function( jwt_payload , done ) {
+        console.log('payload received', jwt_payload);
+        User.find({ id: jwt_payload.id }, function( err , user ) {
+            if ( err ) return done( err , false )
+            if ( user ) done( null , user )
+            else done( null , false )
+        })
+    }))
+}

4d9f7914215be501d6d2386a53bfeb2499b2961b
develdoe <andreeray@live.com>
2018-10-19 08:37:37 +0200

    'u'

--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 
 var todoSchema = new mongoose.Schema({
-    title: { type: String, required: true  },
+    title: { type: String, required: true, unique: false },
     completed: { type: Boolean, required: true },
     user_id: { type: String, required: true }
 })

6f3f62b5da5a6b5f0638690c4d15762708adc3c7
develdoe <andreeray@live.com>
2018-10-21 00:43:17 +0200

    'u'

--- routes.js
+++ routes.js
@@ -194,6 +194,7 @@ module.exports = function ( api ) {
             }
             if(req.body.fname) user.fname = req.body.fname
             if(req.body.lname) user.lname = req.body.lname
+            console.log(req.body.username)
             if(req.body.username) user.username = req.body.username
             user.email = req.body.email
             user.password = req.body.password

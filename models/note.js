c46ff2bfec19b895381fb8585f0bb3b171f1b016
develdoe <andreeray@live.com>
2018-10-21 03:19:52 +0200

    'u'

--- models/note.js
+++ models/note.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose')
 
 var noteSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    completed: { type: Boolean, required: true },
     user_id: { type: String, required: true }
 })
 

7689304475ce1a7213c629f039d922680becb8dc
develdoe <andreeray@live.com>
2018-10-22 15:56:04 +0200

    'u'

--- middleware.js
+++ middleware.js
@@ -16,7 +16,7 @@ module.exports = function( api ) {
 }
 
 function authenticate(req, res, next) {
-    if(/(login)/.test(req.originalUrl)) {
+    if(/(login)|(images)/.test(req.originalUrl)) {
         next()
     } else {
         (passport.authenticate('jwt', { session: false}), function(req, res, next) {

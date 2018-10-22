0c359249dd374e933ad324d5d65190a0e276cb9e
develdoe <andreeray@live.com>
2018-10-22 14:22:57 +0200

    'u'

--- routes.js
+++ routes.js
@@ -16,11 +16,19 @@ const storage = multer.diskStorage({
     }
 })
 
+const fileFilter = (req, file, cb) => {
+    if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true)
+    }
+    else {
+        cb(null, false)
+    }
+}
 
 const upload = multer({
     storage: storage,
     limits: { fileSize: 1024 * 1024 * 5  }, // 5 MB
-
+    fileFilter: fileFilter
 })
 
 module.exports = function ( api ) {

1a810d32dc982e865eb4b75ed644773631c07030
develdoe <andreeray@live.com>
2018-10-22 22:18:15 +0200

    'u'

--- routes.js
+++ routes.js
@@ -66,11 +66,7 @@ module.exports = function ( api ) {
 
     // #################   IMAGES
     api.post('/image', upload.single('img_src'),( req, res ) => {
-        if( err ) {
-            error( res, err )
-            return
-        }
-        res.json({ path: req.file.path})
+        res.json({ path: req.file})
     })
     api.post('/images', upload.array('avatari', 30), (req,res) => {
         console.log(req)

3dfbfd34bba2b258f4cc12e51c78d6b0c15e6894
develdoe <andreeray@live.com>
2018-10-22 22:54:31 +0200

    'u'

--- routes.js
+++ routes.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 const fileFilter = (req, file, cb) => {
     // accept image only
     if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-        return cb(new Error('Only image files are allowed!'), false);
+        return cb(new Error('Only image files are allowed!'));
     }
     cb(null, true)
 }

05309dd23e7c5e0c787a261855a0196e3bc8141a
develdoe <andreeray@live.com>
2018-10-25 18:20:53 +0200

    added ports

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 module.exports = {
-    'secret': 'JI21ko87.HJ/(@df34',
-    'database': 'mongodb://localhost:27017/develstrap'
+    secret: 'JI21ko87.HJ/(@df34',
+    database: 'mongodb://localhost:27017/develstrap',
+    port: '4000'
 }
--- devel-admin-api.js
+++ devel-admin-api.js
@@ -2,11 +2,10 @@ var express  = require( 'express' ),
     api      = express(),
     mongoose = require( 'mongoose' ),
     routes   = require( './routing' )( api ),
-    config   = require('./config'),
-    fs       = require('fs')
+    config   = require('./config')
 
 api.use('/uploads', express.static('uploads'))
-var port = process.env.PORT || 4002
+var port = process.env.PORT || config.port
 mongoose.connect(config.database, { useNewUrlParser: true } )
 api.listen( port )
 console.log( 'http://localhost:', port )

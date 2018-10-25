d71fe2581021e4c17efb39c51ee70020553c59c8
develdoe <andreeray@live.com>
2018-10-25 21:30:51 +0200

    if no user exist create

--- routes.js
+++ routes.js
@@ -34,27 +34,22 @@ module.exports = function ( api ) {
 
     // #################   AUTHENTICATION
     api.post('/login', (req, res) => {
-        User.find({ name: 'root' }, (err, user) => {
-            if(err) {
-                res.json( { err: 'Server ' + err } )
-                return
-            }
-            if (!user.length) {
-                var root = new User()
-                root.name = 'root'
-                root.password = 'toor'
-                root.save(err => {
-                    if( err ) {
-                        error( res, err )
-                        return
-                    }
-                })
-            }
-        })
         User.findOne({ 'email': req.body.email }, (err, user) => {
             if(err) {
                 error(res, err)
                 return
+            }
+            if(user.length === 0) {
+
+                var root = new User()
+                root.name = 'root'
+                root.password = 'toor'
+                root.save(err => {
+                    if (err) {
+                        error(res, err)
+                        return
+                    }
+                })
             }
             if(!user) {
                 res.json({ msg: 'User not found!' })

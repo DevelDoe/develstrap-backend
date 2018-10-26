58813638804178cdaa0c91b8bfc18d698cda33dc
develdoe <andreeray@live.com>
2018-10-26 02:52:30 +0200

    u

--- routes.js
+++ routes.js
@@ -204,10 +204,10 @@ module.exports = function ( api ) {
         var user = new User()
         user.fname = req.body.fname
         user.lname = req.body.lname
-        if(req.body.username === '' ) user.username = req.body.username
+        if(!req.body.username === '' ) user.username = req.body.username
         user.email = req.body.email
         user.password = req.body.password
-        if(req.file.path === '') user.img_src = 'https://media.giphy.com/media/Im7Adiayxy6zK/giphy.gif'
+        if (req.body.img_src === '') user.img_src = 'https://media.giphy.com/media/Im7Adiayxy6zK/giphy.gif'
         user.img_src = req.body.img_src
         user.sec_lv = req.body.sec_lv
         user.applications = req.body.applications

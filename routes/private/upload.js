638a1c3cbeeddd0ff484d2a1f29d1f2d900e44d7
develdoe <andreeray@live.com>
2018-11-16 00:49:47 +0100

    u

--- routes/private/upload.js
+++ routes/private/upload.js
@@ -41,7 +41,7 @@ module.exports = function (api) {
     api.post('/image', uploadImage.single('file'), async (req, res) => {
         try {
             await sharp(req.file.path)
-                .resize(100)
+                .resize(300)
                 .background('white')
                 .embed()
                 .toFile(`./static/${req.file.originalname}`)

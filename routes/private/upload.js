6a6d7e518d7816fbb9a0ed7483a80d25f8b535fa
develdoe <andreeray@live.com>
2018-11-17 13:53:30 +0100

    u

--- routes/private/upload.js
+++ routes/private/upload.js
@@ -56,7 +56,7 @@ module.exports = function (api) {
         try {
             await sharp(req.file.path)
                 .resize(35,35)
-                .background('transparent')
+                .background('rgba(0,0,0,0)')
                 .embed()
                 .toFile(`./uploads/images/processed/${req.file.originalname}`)
             res.json({ file: `/uploads/images/processed/${req.file.originalname}` })

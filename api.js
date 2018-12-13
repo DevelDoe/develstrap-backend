7cbc774e04b4329e891db6f23dda97111c40d13b
develdoe <andreeray@live.com>
2018-12-13 23:59:28 +0100

    u

--- api.js
+++ api.js
@@ -37,71 +37,78 @@ socket.on('connection', (ws, req) => {
     ws.isAlive = true
     ws.on('pong', heartbeat)
 
-    socket.clients.forEach((ws) => {
+    if(ws.type === 'view') {
 
-        var id = setInterval(function () {
-            ws.ss++
-        }, 1000)
+        socket.clients.forEach((ws) => {
 
-        const index = req.connection.remoteAddress.lastIndexOf(':')
-        const ip = req.connection.remoteAddress.substr(index + 1, req.connection.remoteAddress.length)
-        ws.ip = ip
+            var id = setInterval(function () {
+                ws.ss++
+            }, 1000)
 
-        
+            const index = req.connection.remoteAddress.lastIndexOf(':')
+            const ip = req.connection.remoteAddress.substr(index + 1, req.connection.remoteAddress.length)
+            ws.ip = ip
 
-        ws.on('close', function () {
 
-            console.log('close')
 
-            axios.get('http://ip-api.com/json/' + ip).then(res => {
+            ws.on('close', function () {
 
-                let visitor = new Visitor()
-                visitor.ip = ws.ip
-                visitor.city = res.data.city
-                visitor.country = res.data.country
-                visitor.region = res.data.regionName 
-                visitor.timezone = res.data.timezone
-                visitor.date = moment().unix()
-                visitor.seconds = ws.ss
-                visitor.page = ws.page
-                visitor.app = ws.app
-                visitor.user_id = ws.user_id
-                visitor.resolution = ws.resolution
-                visitor.save(err => {
-                    if (err) {
-                        error(res.err)
-                        return
-                    }
-                    console.log('view data added:', visitor.ip)
+                console.log('closing view')
+
+                axios.get('http://ip-api.com/json/' + ip).then(res => {
+
+                    let visitor = new Visitor()
+                    visitor.ip = ws.ip
+                    visitor.city = res.data.city
+                    visitor.country = res.data.country
+                    visitor.region = res.data.regionName
+                    visitor.timezone = res.data.timezone
+                    visitor.date = moment().unix()
+                    visitor.seconds = ws.ss
+                    visitor.page = ws.page
+                    visitor.app = ws.app
+                    visitor.user_id = ws.user_id
+                    visitor.resolution = ws.resolution
+                    visitor.save(err => {
+                        if (err) {
+                            error(res.err)
+                            return
+                        }
+                        console.log('view data added:', visitor.ip)
+                        clearInterval(id)
+                        clearInterval(interval)
+                        ws.terminate()
+                    })
+
+                }).catch(err => {
+                    console.log('ip-api fetch error:')
                     clearInterval(id)
                     clearInterval(interval)
                     ws.terminate()
                 })
-
-                
-
-            }).catch(err => {
-                console.log('ip-api fetch error:')
-                clearInterval(id)
-                clearInterval(interval)
-                ws.terminate()
             })
-            
-            
         })
-    })
+    }
+
+    
 
     ws.on('message', (msg) => {
 
-        console.log(msg)
-
         parsed = JSON.parse(msg)
-        
-        ws.ss = 0
-        ws.page = parsed.page
-        ws.app = parsed.app
-        ws.user_id = parsed.user_id
-        ws.resolution = parsed.resolution
+
+        switch(parsed.type) {
+            case 'view':
+                ws.type = 'view'
+                ws.ss = 0
+                ws.page = parsed.page
+                ws.app = parsed.app
+                ws.user_id = parsed.user_id
+                ws.resolution = parsed.resolution
+                break
+            case 'forum':
+                console.log(parsed)
+        }
+
     })
 
 })
@@ -112,7 +119,6 @@ const interval = setInterval(function ping() {
             console.log('dead')
             return ws.terminate()
         }
-
         ws.isAlive = false
         ws.ping(noop)
     })

cbd47ef8d45f7a797843aa4c509841c71cdc58bd
develdoe <andreeray@live.com>
2018-12-15 22:12:31 +0100

    u

--- models/message.js
+++ models/message.js
@@ -4,5 +4,5 @@ module.exports = mongoose.model('Message', {
      room           : { type: String },
      user           : { type: String },
      message        : { type: String },
-     created_ad     : { type: String },
+     created_at     : { type: String },
 })

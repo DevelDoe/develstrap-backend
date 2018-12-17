9df9c75e634851bf39c7010f3a220ec57ff70418
develdoe <andreeray@live.com>
2018-12-17 03:54:13 +0100

    u

---
+++ models/ticket.js
@@ -0,0 +1,33 @@
+var mongoose = require('mongoose')
+module.exports = mongoose.model('Ticket', {
+     title: {
+          type: String
+     },
+     category: {
+          type: Array
+     },
+     steps: {
+          type: Array
+     },
+     created_at: {
+          type: String
+     },
+     updated_at: {
+          type: String
+     },
+     deleted: {
+          type: Boolean
+     },
+     result: {
+          type: String
+     },
+     expected: {
+          type: String
+     }, 
+     workaround: {
+          type: String
+     },
+     repro: {
+          type: Array
+     }
+})
---
+++ routes/private/tickets.js
@@ -0,0 +1,76 @@
+const Ticket       = require('../../models/ticket')
+const { error }    = require('../../hlps')
+const Moment       = require('moment')
+
+module.exports = api => {
+
+     api.get('/tickets', (req, res) => {
+
+          Ticket.find( ( err, tickets ) => {
+               if ( err ) {
+                    error( res, err )
+                    return
+               }
+               res.json( tickets )
+          })
+
+     })
+
+     api.post( '/tickets', ( req, res ) => {
+
+          var ticket = new Ticket() 
+
+          ticket.title             = req.body.title 
+          ticket.category          = req.body.category 
+          ticket.steps             = req.body.steps 
+          ticket.created_at        = Moment().unix() 
+          ticket.deleted           = req.body.deleted 
+          ticket.result            = req.body.result
+          ticket.expected          = req.body.expected
+          ticket.workaround        = req.body.workaround 
+          ticket.repro             = req.body.repro 
+
+          ticket.save( err => {
+               if( err ) {
+                    error( res, err )
+                    return
+               }
+               res.json( ticket )
+          })
+
+     })
+
+     api.post( '/tickets/:_id', ( req, res ) => {
+
+          Ticket.findById( req.params._id, ( err, ticket ) => {
+
+               if( err ) {
+                    error( res, err )
+                    return
+               }
+
+               var ticket = new Ticket() 
+
+               ticket.title             = req.body.title 
+               ticket.category          = req.body.category 
+               ticket.steps             = req.body.steps 
+               ticket.updated_at        = Moment().unix() 
+               ticket.deleted           = req.body.deleted 
+               ticket.result            = req.body.result
+               ticket.expected          = req.body.expected
+               ticket.workaround        = req.body.workaround 
+               ticket.repro             = req.body.repro 
+
+               ticket.save( err => {
+                    if( err ) {
+                         error( res, err )
+                         return
+                    }
+                    res.json( ticket )
+               })
+
+          })
+
+     })
+
+}

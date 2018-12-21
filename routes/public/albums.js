6279d870a42a30bef1b8fe428d1ae406306b752f
develdoe <andreeray@live.com>
2018-12-21 01:49:23 +0100

    u

--- routes/public/albums.js
+++ routes/public/albums.js
@@ -1,4 +1,5 @@
 const Album = require('../../models/image')
+const User = require('../../models/user')
 const { error } = require('../../hlps')
 module.exports = api => {
 
@@ -8,7 +9,20 @@ module.exports = api => {
                     error( res, err )
                     return
                }
-               res.json( albums )
+               let users
+               User.find( ( er, u ) => {
+                    if (err) {
+                         error(res, err)
+                         return
+                    }
+                    users = u
+               })
+               let modAlbums = JSON.parse(JSON.stringify(albums))
+               modAlbums.forEach( album => {
+                    album.artist = users.find(user => user._id === album.user_id)
+               })
+
+               res.json( modAlbums )
           })
      })
 

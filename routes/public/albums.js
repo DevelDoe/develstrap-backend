df960afb45d758e4f661203848aed71efe432305
develdoe <andreeray@live.com>
2018-12-21 02:04:53 +0100

    u

--- routes/public/albums.js
+++ routes/public/albums.js
@@ -17,9 +17,9 @@ module.exports = api => {
                     let modAlbums = JSON.parse(JSON.stringify(albums))
                     modAlbums.forEach(album => {
                          console.log('album.user_id ' + album.user_id)
-                         const artist = users.find(user => {
+                         const artist = users.forEach(user => {
                               console.log('user._id ' + user._id)
-                              user._id === album.user_id
+                              if(user._id === album.user_id) return user.username
                          })
                          console.log(artist)
                     })

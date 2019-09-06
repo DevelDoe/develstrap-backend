84ec098b8a96111afa3995d52901cc855f9cc760
develdoe <andreeray@live.com>
2019-09-06 13:28:50 +0200

    u

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     secret: 'JI21ko87.HJ/(@df34',
-    database: 'mongodb://localhost:27017/develdevils',
+    database: 'mongodb://localhost:27017/develadmin',
     port: '4000' 
 }

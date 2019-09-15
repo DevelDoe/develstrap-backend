6668aa443b1bcca032c6359c9b74b7793ae2b0f0
develdoe <andreeray@live.com>
2019-09-15 19:08:29 +0200

    u

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose')
 module.exports = mongoose.model('Workout', {
      group       : { type: String, required: true },
-     name        : { type: String },
+     name        : { type: String, required: true },
      exercise_id : { type: String, required: true },
      level       : { type: Number, required: true },
      weight      : { type: Number, required: true },

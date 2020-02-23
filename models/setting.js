08ebe77450d37f3cedc3aa020b1b919b7d211e37
develdoe <andreeray@live.com>
2020-02-23 19:24:23 +0100

    u

--- models/setting.js
+++ models/setting.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
 module.exports = mongoose.model('Setting', {
     user_id:    { type: String },
-    sites:      { type: Object }
+    sites:      { type: Array }
 })
